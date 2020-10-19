a91cf2235c4b23678cb38d7de556afd673359700
boris <borzor@mail.ru>
2020-10-19 15:42:31 -0700

    few changes

--- MONG_CURVE/main.cpp
+++ MONG_CURVE/main.cpp
@@ -6,9 +6,9 @@ point point1;
 NTL::ZZ k1,k2;
 RandomBnd(k1,point1.p);
 RandomBnd(k2,point1.p);
-test.is_point_on_curve(point1);
-test.qP_(point1);
 test.qP_second(point1);
+test.qP_(point1);
+test.is_point_on_curve(point1);
 test.distributivity(point1, k1, k2);
 return 0;
 }
--- MONG_CURVE/tests.h
+++ MONG_CURVE/tests.h
@@ -13,11 +13,12 @@ public:
         MulMod(t3, t3, point1.a, point1.p);
         AddMod(t2, t2, t3, point1.p);
         AddMod(t2, t2, point1.X, point1.p);
+        std::cout<<"checking if point on curve\nX = "<<point1.X<<"\nZ = "<<point1.Z<<'\n';
         if(compare(t1,t2)==0){
-            std::cout<<"point on curve\n";
+            std::cout<<"point on curve\n\n";
         }
         else{
-            std::cout<<"point not on curve\n";
+            std::cout<<"point not on curve\n\n";
         }
     }
     void distributivity(point &point1, NTL::ZZ k1, NTL::ZZ k2){
@@ -34,33 +35,37 @@ public:
         cmp.montgomery_ladder(point5, k1-k2);
         cmp.add_point(point2,point3,point5);
         cmp.point_transform(point2);
+        std::cout<<"checking K1*P + K2*P = (K1+K2)*P\n";
         if(!compare(point2.X, point4.X)){
-            std::cout<<"distributivity is fine\n";
+            std::cout<<"Test passed\n";
         }
         else{
-            std::cout<<"distributivity is not fine\n";
+            std::cout<<"Test failed\n";
         }
     }
     void qP_(point &point1){
         point point_check(point1);
         cmp.montgomery_ladder(point_check, point1.q);
+        std::cout<<"Point with Q multiplier\n";
         if(point_check.X==1 && point_check.Z==0){
-            std::cout<<"new point is neutral\n";
+            std::cout<<"new point is neutral\n\n";
         }
     }
     void qP_second(point &point1){
         point point_plus(point1);
-        point point_munus(point1);
+        point point_minus(point1);
         NTL::ZZ one(1);
-        std::cout<<"default point after transformations from GOST\n"<<point1.X<<'\n'<<point1.Z<<'\n';
+        std::cout<<"default point after transformations from GOST\nX = "<<point1.X<<"\nZ = "<<point1.Z<<"\n\n";
         AddMod(point_plus.q,point_plus.q, one, point1.p);
         cmp.montgomery_ladder(point_plus,point_plus.q);
         if (!compare(point_plus.X, point1.X)&&!compare(point_plus.Z, point1.Z)){
-            std::cout<<"Point with Q+1 multiplier\nTest passed\n";
+            std::cout<<"Point with Q+1 multiplier\nX = "<<point_minus.X<<"\nZ = "<<point_minus.Z<<'\n'<<"Test passed\n";
         }
-        SubMod(point_munus.q,point_munus.q, one, point1.p);
-        cmp.montgomery_ladder(point_munus,point_munus.q);
-        std::cout<<"Point with q-1 multiplier\n"<<point_munus.X<<'\n'<<point_munus.Z<<'\n';
+        SubMod(point_minus.q,point_minus.q, one, point1.p);
+        cmp.montgomery_ladder(point_minus,point_minus.q);
+        if (!compare(point_minus.X, point1.X)&&!compare(point_minus.Z, point1.Z)){
+            std::cout<<"Point with q-1 multiplier\nX = "<<point_minus.X<<"\nZ = "<<point_minus.Z<<'\n'<<"Test passed\n\n";
+        }
     }
 
 
